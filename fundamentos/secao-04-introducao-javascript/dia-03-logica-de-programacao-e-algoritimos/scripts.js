089559de8d423035b0e099d993a50b6e92910d42
Carame Júnior <caramejr@gmail.com>
2022-10-25 19:34:12 -0300

    novo exercicio adicionado

--- fundamentos/secao-04-introducao-javascript/dia-03-logica-de-programacao-e-algoritimos/scripts.js
+++ fundamentos/secao-04-introducao-javascript/dia-03-logica-de-programacao-e-algoritimos/scripts.js
@@ -21,3 +21,14 @@ for (let i = 0; i < array.length; i += 1) {
     console.log('Menor palavra: ' + menorPalavra);
   }
 }
+
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. 
+// Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, 
+// depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let palavra = 'tryber';
+let reversePalavra = '';
+for (let i = 0; i < palavra.length; i += 1){
+  reversePalavra += palavra[palavra.length - 1 - i];
+}
+console.log(reversePalavra);
