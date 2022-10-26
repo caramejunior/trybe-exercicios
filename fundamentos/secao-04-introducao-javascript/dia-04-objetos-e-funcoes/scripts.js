17a858781c5151888c1fd65b055c4b037da60186
Carame Júnior <caramejr@gmail.com>
2022-10-26 10:33:37 -0300

    novo exercicio

--- fundamentos/secao-04-introducao-javascript/dia-04-objetos-e-funcoes/scripts.js
+++ fundamentos/secao-04-introducao-javascript/dia-04-objetos-e-funcoes/scripts.js
@@ -38,3 +38,15 @@ let car = {
 for (let key in car){
   console.log(key, car[key]);
 }
+
+// Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (let key in names){
+  console.log('Olá ' + names[key] + '.')
+}
